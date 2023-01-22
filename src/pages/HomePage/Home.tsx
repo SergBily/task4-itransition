c5d8946c364a1c2e03828ec39f23d837746e1c1d
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-22 16:00:13 +0300

    feat: implement home page and not found page

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './home.scss';
 
 const Home: React.FC = (): JSX.Element => (
-  <div>Hello user</div>
+  <div className="home__container">
+    <h1 className="home__title"> WELCOME TO USER MANAGEMENT APP!</h1>
+  </div>
 );
 
 export default Home;
