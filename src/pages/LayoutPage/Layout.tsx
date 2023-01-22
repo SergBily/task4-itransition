8809c855e7054b3eec55177bc8ab7087570bfe75
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-22 15:59:07 +0300

    fix: layout for app

--- src/pages/LayoutPage/Layout.tsx
+++ src/pages/LayoutPage/Layout.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './layout.scss';
 import { Outlet } from 'react-router-dom';
-import Header from '../../components/Header/Header';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 const Layout: React.FC = (): JSX.Element => (
-  <>
+  <div className="app__wrapper">
     <Header />
     <div className="wrapper">
       <Outlet />
     </div>
-  </>
+    <Footer />
+  </div>
 );
 
 export default Layout;
