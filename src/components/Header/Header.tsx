2f3632c899753ec851744379125d39ab79d51d3c
SergBily <sergBily.dev@gmail.com>
2023-01-22 12:48:27 +0300

    fix: header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,51 @@
 import React from 'react';
 import './header.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../redux/features/authSlice';
+import { AppDispatch, RootState } from '../../redux/Store';
 
-const Header: React.FC = (): JSX.Element => (
-  <div className="header__wrapper">
-    <div className="header__container">
-      <h1>UserManagement</h1>
-      <nav className="header__nav">
-        <Link to="auth" className="header__link">
-          <Button variant="contained" size="medium" className="header__btn">
-            Sign in
-          </Button>
-        </Link>
-        <Link to="registration" className="header__link">
-          <Button variant="contained" size="medium" className="header__btn">
-            Sign up
-          </Button>
-        </Link>
-        <Link to="management" className="header__link">
-          <Button variant="contained" size="medium" className="header__btn">
-            management
-          </Button>
-        </Link>
-      </nav>
+const Header: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
+
+  return (
+    <div className="header__wrapper">
+      <div className="header__container">
+        <h1 className="header__title">UserManagement</h1>
+        <nav className="header__nav">
+          {user ? (
+            <Button variant="contained" size="medium" className="header__btn" onClick={onLogout}>
+              Logout
+            </Button>
+          ) : (
+            <>
+              <Link to="login" className="header__link">
+                <Button variant="contained" size="medium" className="header__btn">
+                  Sign in
+                </Button>
+              </Link>
+              <Link to="registration" className="header__link">
+                <Button variant="contained" size="medium" className="header__btn">
+                  Sign up
+                </Button>
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
